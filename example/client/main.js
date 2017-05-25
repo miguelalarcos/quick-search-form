5564f22c09a41d7e5540a2dadc0c2809d504aee4
Miguel Ángel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2017-05-25 09:20:17 +0200

    example with form

--- example/client/main.js
+++ example/client/main.js
@@ -4,6 +4,27 @@ import 'meteor/quick-search-form';
 
 import './main.html';
 
+extractValidation = (schema) => {
+  ret = {};
+  for(let k of Object.keys(schema)){
+    ret[k] = (v)=>{
+      return schema[k].validate(v);
+    }
+  }
+  return ret;
+}
+
+const isBlank = (x)=>{return x == undefined || x == null || x == ''}
+
+const schema_form = {
+      a: {type: 'integer', name: 'a',  desc: 'A:', validate: (v) => {
+        if(!isBlank(v)){console.log(v,+v);return (+v > 5) || 'a debe ser mayor que 5'}
+        return true;
+      }
+    },
+      b: {type: 'string', name: 'b',  desc: 'B:', validate: (v)=>{return !isBlank(v) || 'obligatorio';}},
+    };
+
 Template.hello.helpers({
   schema() {
     return {
@@ -31,6 +52,23 @@ Template.hello.helpers({
   },
   repr(){
     return JSON.stringify(Session.get('output'));
+  },
+  schema_form() {
+    return schema_form;
+  },
+  validation(){
+    return extractValidation(schema_form);
+  },
+  initial_form() {
+    return {a: 5};
+  },
+  fields(){
+    return [{type: 'integer', desc: 'A:', name: 'a'},
+            {type: 'string', desc: 'B:', name: 'b'}
+    ];
+  },
+  repr3(){
+    return JSON.stringify(Session.get('output3'));
   }
 });
 
