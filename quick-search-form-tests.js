b830b8c818db2b2312175fe31405653a1b747524
Miguel Ángel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2017-06-04 12:26:52 +0200

    more tests

--- quick-search-form-tests.js
+++ quick-search-form-tests.js
@@ -1,27 +1,49 @@
 import { Tinytest } from "meteor/tinytest";
 //import {  } from "meteor/miguelalarcos:quick-search-form";
-import { queryJSON2Mongo } from './utils.js';
+import { queryJSON2Mongo, form2JSON, JSON2form } from './utils.js';
 
-Tinytest.add('mmm - query simple one parameter', function (test) {  
+Tinytest.add('queryJSON2Mongo - query simple one parameter', function (test) {  
   const queryJSON = {"a$gt": 5};
   const expected = {a: {$gt: 5}};
   const mongoQ = queryJSON2Mongo(queryJSON);
-  const flag = _.isEqual(mongoQ, expected);
-  test.isTrue(flag);
+  test.equal(mongoQ, expected);
 });
 
-Tinytest.add('mmm - query two parameters', function (test) {  
+Tinytest.add('queryJSON2Mongo - query two parameters', function (test) {  
   const queryJSON = {"a$gt": 5, "b$eq": 'hello'};
   const expected = {a: {$gt: 5}, b: {$eq: 'hello'}};
   const mongoQ = queryJSON2Mongo(queryJSON);
-  const flag = _.isEqual(mongoQ, expected);
-  test.isTrue(flag);
+  test.equal(mongoQ, expected);
 });
 
-Tinytest.add('mmm - query two parameters one field', function (test) {  
+Tinytest.add('queryJSON2Mongo - query two parameters one field', function (test) {  
   const queryJSON = {"a$gt": 5, "a$lt": 10};
   const expected = {a: {$gt: 5, $lt: 10}};
   const mongoQ = queryJSON2Mongo(queryJSON);
-  const flag = _.isEqual(mongoQ, expected);
-  test.isTrue(flag);
+  test.equal(mongoQ, expected);
+});
+
+const schema = {x: {type: 'integer'}, d: {type: 'decimal'}}
+
+Tinytest.add('form2JSON JSON2form - simple', function (test) {  
+  const formDoc = {x: 5, d: 1};
+  const JSONDoc = form2JSON(formDoc, schema);
+  const expected = JSON2form(JSONDoc, schema);  
+  test.equal(formDoc, expected);
+});
+
+const schemaNested = {'x-y-z': {type: 'integer'}}
+
+Tinytest.add('form2JSON JSON2form - nested', function (test) {  
+  const formDoc = {'x-y-z': 5};
+  const JSONDoc = form2JSON(formDoc, schemaNested);
+  const expected = JSON2form(JSONDoc, schemaNested);  
+  test.equal(formDoc, expected);
+});
+
+Tinytest.add('form2JSON - nested', function (test) {  
+  const formDoc = {'x-y-z': 5};
+  const JSONDoc = form2JSON(formDoc, schemaNested);
+  const expected = {x:{y:{z:5}}};
+  test.equal(JSONDoc, expected);
 });
